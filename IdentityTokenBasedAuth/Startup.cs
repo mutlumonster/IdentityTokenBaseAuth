bb88e632f67fe263e364fdad9d7b0ee5b6aee9e8
mutluwindows <58912576+mutluwindows@users.noreply.github.com>
2021-02-26 18:33:32 +0400

    aaa

--- IdentityTokenBasedAuth/Startup.cs
+++ IdentityTokenBasedAuth/Startup.cs
@@ -1,17 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using IdentityTokenBasedAuth.Models;
+using IdentityTokenBasedAuth.Security.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using IdentityTokenBasedAuth.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace IdentityTokenBasedAuth
 {
@@ -44,23 +40,47 @@ namespace IdentityTokenBasedAuth
 
             }).AddEntityFrameworkStores<AppIdentityDbContext>();
 
+            services.Configure<CustomTokenOptions>(Configuration.GetSection("TokenOptions"));
+            var tokenOptions = Configuration.GetSection("TokenOptions").Get<CustomTokenOptions>();
+
+            services.AddAuthentication(opts =>
+            {
+                opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(jwtbeareroptions =>
+                jwtbeareroptions.TokenValidationParameters =
+                    new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                    {
+                        ValidateAudience = true,
+                        ValidateIssuer = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidAudience = tokenOptions.Audience,
+                        //IssuerSigningKey = SignHandler
+                        ClockSkew = TimeSpan.Zero
+                    };
+        });
+
+
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseMvc();
+    }
+}
 }
